fc9d920f4fbad354cebade73613f68140f6dce11
Ahmad MOHADDES <mohaddespour@gmail.com>
2019-02-07 15:26:40 -0500

    Added entries and list

--- library.py
+++ library.py
@@ -1,28 +1,37 @@
-import sqlite3
+from tkinter import *
 
-# 5 steps to connect to data base
-conn = sqlite3.connect("lite.db")
-cur = conn.cursor()
-cur.execute("CREATE TABLE IF NOT EXISTS store (item TEXT, quantity INTEGER, price REAL)")
-conn.commit()
-conn.close()
+window = Tk()
 
-def insert(item,quantity,price):
-    conn = sqlite3.connect("lite.db")
-    cur = conn.cursor()
-    cur.execute("INSERT INTO store VALUES(?,?,?)",(item,quantity,price))
-    conn.commit()
-    conn.close()
-    
-insert("iPhone",2,320)
-insert("Samsung",1,650)
-        
-def view():
-    conn = sqlite3.connect("lite.db")
-    cur = conn.cursor()
-    cur.execute("SELECT * FROM store")
-    rows = cur.fetchall()
-    conn.close()
-    return rows
-    
-print(view())
+l1 = Label(window,text="Title")
+l1.grid(row=0,column=0)
+
+l2 = Label(window,text="Author")
+l2.grid(row=0,column=2)
+
+l3 = Label(window,text="Year")
+l3.grid(row=1,column=0)
+
+l4 = Label(window,text="ISBN")
+l4.grid(row=1,column=2)
+
+title_text=StringVar()
+e1=Entry(window,textvariable = title_text)
+e1.grid(row=0,column=1)
+
+author_text=StringVar()
+e2=Entry(window,textvariable = author_text)
+e2.grid(row=0,column=3)
+
+year_text=StringVar()
+e3=Entry(window,textvariable = year_text)
+e3.grid(row=1,column=1)
+
+isbn_text=StringVar()
+e4=Entry(window,textvariable = isbn_text)
+e4.grid(row=1,column=3)
+
+list1=Listbox(window, height=6,width=35)
+list1.grid(row=2,column=0,rowspan=6,columnspan=2)
+
+
+window.mainloop()
