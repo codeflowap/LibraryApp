bbece560e17ce443f99be7245f6357f5e03d78e5
Ahmad MOHADDES <mohaddespour@gmail.com>
2019-02-07 16:08:44 -0500

    Backend - created insert func

--- backenedLibrary.py
+++ backenedLibrary.py
@@ -6,5 +6,13 @@ def connect():
     cur.execute("CREATE TABLE IF NOT EXISTS book (id INTEGER  PRIMARY KEY, title text, author text, year INTEGER, isbn INTEGER)")
     conn.commit()
     conn.close()
+
+def insert(title,author,year,isbn):
+    conn=sqlite3.connect("book.db")
+    cur=conn.cursor()
+    cur.execute("INSERT INTO book VALUES (NULL,?,?,?,?)",(title,author,year,isbn))
+    conn.commit()
+    conn.close()
+    
     
 connect()
